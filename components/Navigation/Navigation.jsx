cff0dbf5f3ffa22be359630a3ff41af99a4cfa37
adiwahyudi <adi.wahyudi@majoo.id>
2022-02-24 14:27:48 +0700

    add route is active

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,20 +1,23 @@
 import Link from "next/link";
-import styles from "./Navigation.module.scss"
+import { useRouter } from "next/router";
+import styles from "./Navigation.module.scss";
 
 const Navigation = () => {
+    const router = useRouter();
+
     return ( 
         <nav className={styles.navMenu}>
             <Link href="/">
-                <a>Work</a>
+                <a className={router.pathname == "/" ? styles.active : ""}>Work</a>
             </Link>
             <Link href="/studio">
-                <a>Studio</a>
+                <a className={router.pathname == "/studio" ? styles.active : ""}>Studio</a>
             </Link>
             <Link href="/play">
-                <a>Play</a>
+                <a className={router.pathname == "/play" ? styles.active : ""}>Play</a>
             </Link>
             <Link href="/contact">
-                <a>Contact</a>
+                <a className={router.pathname == "/contact" ? styles.active : ""}>Contact</a>
             </Link>
             <div className={styles.dot} />
         </nav>
